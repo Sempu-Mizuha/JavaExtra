454c2103f621c9d4025bc45932135ad85f416fbd
Sempu-Mizuha <sempumizuha@yahoo.co.jp>
2017-12-31 21:23:06 +0900

    競合させます。

--- src/jp/miyabilink/atm/BankCode.java
+++ src/jp/miyabilink/atm/BankCode.java
@@ -9,7 +9,31 @@ package jp.miyabilink.atm;
  * @author Sempu-Mizuha
  * @version 1.0
  * @since 1.0 Sempu-Mizuha 2017/12/31 17:26:59 <br>
+ * @since 1.1 Sempu-Mizuha 2017/12/31 20:59:58 <br>
  */
 public class BankCode {
+  /**
+   * 銀行コード.
+   * @since 1.1 Sempu-Mizuha 2017/12/31 21:09:43 <br>
+   */
+  private String number;
+
+  /**
+   * numberを取得するメソッド.
+   * @return numberを取得する
+   * @since 1.1 Sempu-Mizuha 2017/12/31 20:59:58 <br>
+   */
+  public String getNumber() {
+    return number;
+  }
+
+  /**
+   * numberを登録するメソッド.
+   * @param number numberを登録する
+   * @since 1.1 Sempu-Mizuha 2017/12/31 20:59:58 <br>
+   */
+  public void setNumber(final String number) {
+    this.number = number;
+  }
 
 }
