ad3d48cd285040ff820618c6d495d5878adf9cb9
Sempu-Mizuha <sempumizuha@yahoo.co.jp>
2017-12-31 19:33:19 +0900

    練習

---
+++ README.md
@@ -0,0 +1,5 @@
+# JavaExtra
+スッキリわかるJava入門実践編、第16章の練習のために作成します。SCNのサーバを立てれないので代用です。ブランチは今回利用しません。
+
+※Javadoc<br>
+https://sempu-mizuha.github.io/JavaExtra/index.html
---
+++ src/jp/miyabilink/atm/BankCode.java
@@ -0,0 +1,15 @@
+/**
+ * パッケージ.クラス名:
+ * jp.miyabilink.atm.BankCode
+ */
+package jp.miyabilink.atm;
+
+/**
+ * 銀行のコードを登録する.
+ * @author Sempu-Mizuha
+ * @version 1.0
+ * @since 1.0 Sempu-Mizuha 2017/12/31 17:26:59 <br>
+ */
+public class BankCode {
+
+}
